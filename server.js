887e36c1bcc4f904a3980dc5a12c936a1d8aad79
Dan Morris <dan@dmonline.uk>
2025-06-28 21:45:36 +0100

    conteplating serving ds.js for visualisations; setup a client script to gather stats

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const server = express();
+const path = require("path");
+
 
 server.get("/", (req, res) => {
-    res.json({message: "Hello world!"});
+    res.sendFile(path.join(__dirname, './index.html'));
 });
 
 server.listen(3000);
